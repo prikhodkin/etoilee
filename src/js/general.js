8b3565a6ab43a52c689a8f26cbf5b90d036e3cb9
SychugovKirill <9090000640@mail.ru>
2020-10-21 19:22:52 +0500

    Добавляет разметку и стили к блоку header

--- src/js/general.js
+++ src/js/general.js
@@ -3,7 +3,7 @@ import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 import Tabs from "%modules%/tabs/tabs"
-import {$, $$} from "./util";
+import { $, $$ } from "./util";
 
 
 Turbolinks.start();
@@ -15,3 +15,17 @@ application.load(definitionsFromContext(context));
 
 const tabs = $(`[data-tabs]`);
 new Tabs(tabs);
+
+//Открыть меню
+let toggleNav = document.querySelector(".header__toggle");
+let mainNav = document.querySelector(".header__nav");
+
+toggleNav.addEventListener("click", function () {
+  if (mainNav.classList.contains("header__nav--close")) {
+    mainNav.classList.remove("header__nav--close");
+    mainNav.classList.add("header__nav--active");
+  } else {
+    mainNav.classList.add("header__nav--close");
+    mainNav.classList.remove("header__nav--active");
+  }
+});
